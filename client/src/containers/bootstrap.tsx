1d4df719723d497b1266f6e61390cb7e9adcdda0
iMoses <imoses.g@gmail.com>
2022-09-06 18:33:32 +0300

    better typewriter font than 18th century

--- client/src/containers/bootstrap.tsx
+++ client/src/containers/bootstrap.tsx
@@ -6,12 +6,18 @@ export default function Bootstrap() {
     <>
       <Title>Cthulhu Wars Online</Title>
       <InteractiveMap />
-      <SpellCard>
-        In any Doom phase in which you have 5 faction Spellbooks, receive this
-        Spellbook.
-        <br />
-        Also receive 1 Elder Sign.
-      </SpellCard>
+      <CardsGrid>
+        <SpellCard>
+          In any Doom phase in which you have 5 faction Spellbooks, receive this
+          Spellbook.
+          <br />
+          Also receive 1 Elder Sign.
+        </SpellCard>
+        <SpellCard>
+          <strong>Devour (Pre-Battle)</strong>: The enemy player eliminates one of
+          his Monsters or Cultists in the area, his choice.
+        </SpellCard>
+      </CardsGrid>
     </>
   );
 }
@@ -19,12 +25,23 @@ export default function Bootstrap() {
 const Title = styled.h1`
   font-size: 48px;
   font-family: var(--font-abaddon);
+  white-space: nowrap;
   margin-bottom: 24px;
 `;
 
+const CardsGrid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+`;
+
 const SpellCard = styled.div`
-  width: 480px;
+  flex: 0 0 480px;
   padding: 12px 16px;
   outline: 1px solid #fff;
   background-color: rgba(0, 0, 0, 0.5);
+
+  strong {
+    font-weight: bold;
+  }
 `;
